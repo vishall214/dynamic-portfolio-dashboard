1804d6dcad0f090d69b7204832f2224b4fbf3124
vishall214 <Vishalnyapathi214@gmail.com>
2026-09-13 01:43:46 +0530

    loading page changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="p-6">
       <h1 className="mb-4 text-2xl font-bold">
-        Dynamic Portfolio Dashboard
+        DynamicPortfolio Dashboard
       </h1>
 
       <PortfolioTable />
